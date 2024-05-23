491f3f0e58e64fe96ce9cc85705e6f1cfef1a22d
SrKain <134458062+SrKain@users.noreply.github.com>
2024-05-23 13:16:01 -0300

    Desenvolvimento de teste para uso da API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:1000/user")
+      .then((response) => response.json())
+      .then((data) => setUser(data))
+      .catch((error) => {
+        console.log("Deu errado situação", error);
+      });
+  }, []);
+
+  console.log(user)
+
+  return <div className="App">{user?<p>deu certo</p>:<p>deu errado</p>}</div>;
 }
-
-export default App;
